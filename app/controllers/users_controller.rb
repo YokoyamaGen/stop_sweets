015a53485b0fdb9354c88ba47b384fd014347ba1
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-04-29 20:59:18 +0900

    マイページ編集画面の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,41 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     use_days = @user.calc_use_day
     @save_money = @user.calc_save_money(use_days)
     @stop_eat_sweets_day = @user.calc_stop_day
   end
 
-  def update_eat_day
-    user = User.find(current_user.id)
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    if user.eat_day_updated_on != Date.today && user.calc_stop_day > 0
-      user.update!(eat_day: user.eat_day + 1, eat_day_updated_on: Date.today)
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "入力に不備があります。"
+      render :edit
+    end
+  end
+
+  def update_eat_day
+    @user = User.find(params[:id])
+
+    if @user.eat_day_updated_on != Date.today && @user.calc_stop_day > 0
+      @user.update!(eat_day: @user.eat_day + 1, eat_day_updated_on: Date.today)
     else
       flash[:alert] = "本日はこれ以上、申告できません"
     end
-    redirect_back(fallback_location: users_path)
+    redirect_back(fallback_location: user_path(@user))
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :cost)
+  end
+
 end
