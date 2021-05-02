d946705483e90476abc937b6c137def658deca98
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-05-02 16:15:54 +0900

    マイページにプロフィール画像の投稿機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  mount_uploader :image, ImageUploader
 
   def calc_use_day
     (Date.current - self.created_at.to_date).to_i
