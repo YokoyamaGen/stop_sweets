d248eb951ac222e7b5d24266bb92fbf97ba9077d
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-05-08 15:20:05 +0900

    マイページの編集画面にバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, length: { maximum: 255 }
+  validates :cost, numericality: { greater_than_or_equal_to: 0 }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
