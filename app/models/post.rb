65c885d0211c060248ffa4cee7321cc6468d2937
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-05-14 16:58:44 +0900

    不要なコードを削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true, length: { maximum: 140 }
 
   def liked_by?(user)
