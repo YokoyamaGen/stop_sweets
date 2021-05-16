64a65f1e395ae04ebcfaa6dfd7989a2d452eda1c
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-05-16 17:19:05 +0900

    Postモデルのモデルスペック(バリデーション)を実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe "バリデーション" do
+    subject { post.valid? }
+    context "データが条件を満たすとき" do
+      let(:post) { build(:post) }
+      it "保存できる" do 
+        expect(subject).to eq true
+      end
+    end
+    context "content が空のとき" do
+      let(:post) { build(:post, content: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(post.errors.messages[:content]).to include "を入力してください"
+      end
+    end
+    context "content  が141文字以上のとき" do
+      let(:post) { build(:post, content: "a" * 141) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(post.errors.messages[:content]).to include "は140文字以内で入力してください"
+      end
+    end
+  end
+
+  context "投稿が削除されたとき" do
+    subject { post.destroy }
+
+    let(:post) { create(:post) }
+    before do
+      create_list(:like, 2, post: post)
+      create(:like)
+    end
+    it "その投稿のいいねも削除される" do
+      expect { subject }.to change { post.likes.count }.by(-2)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,64 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe "バリデーション" do
-      subject { user.valid? }
-      context "データが条件を満たすとき" do
-        let(:user) { build(:user) }
-        it "保存できる" do
-          expect(subject).to eq true
-        end
-      end
-      context "name が空のとき" do
-        let(:user) { build(:user, name: "") }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:name]).to include "を入力してください"
-        end
-      end
-      context "name が51文字以上のとき" do
-        let(:user) { build(:user, name: "a" * 51) }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:name]).to include "は50文字以内で入力してください"
-        end
-      end
-      context "email が空のとき" do
-        let(:user) { build(:user, email: "") }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:email]).to include "を入力してください"
-        end
-      end
-      context "email が256文字以上のとき" do
-        let(:user) { build(:user, email: "a" * 256) }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:email]).to include "は255文字以内で入力してください"
-        end
-      end
-      context "cost が負の整数のとき" do
-        let(:user) { build(:user, cost: -1) }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:cost]).to include "は0以上の値にしてください"
-        end
-      end
-      context "cost が文字列のとき" do
-        let(:user) { build(:user, cost: "a") }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:cost]).to include "は数値で入力してください"
-        end
-      end
-      context "cost が小数点のとき" do
-        let(:user) { build(:user, cost: 2.0) }
-        it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(user.errors.messages[:cost]).to include "は整数で入力してください"
-        end
+  describe "バリデーション" do
+    subject { user.valid? }
+    context "データが条件を満たすとき" do
+      let(:user) { build(:user) }
+      it "保存できる" do
+        expect(subject).to eq true
       end
     end
+    context "name が空のとき" do
+      let(:user) { build(:user, name: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:name]).to include "を入力してください"
+      end
+    end
+    context "name が51文字以上のとき" do
+      let(:user) { build(:user, name: "a" * 51) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:name]).to include "は50文字以内で入力してください"
+      end
+    end
+    context "email が空のとき" do
+      let(:user) { build(:user, email: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:email]).to include "を入力してください"
+      end
+    end
+    context "email が256文字以上のとき" do
+      let(:user) { build(:user, email: "a" * 256) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:email]).to include "は255文字以内で入力してください"
+      end
+    end
+    context "cost が負の整数のとき" do
+      let(:user) { build(:user, cost: -1) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:cost]).to include "は0以上の値にしてください"
+      end
+    end
+    context "cost が文字列のとき" do
+      let(:user) { build(:user, cost: "a") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:cost]).to include "は数値で入力してください"
+      end
+    end
+    context "cost が小数点のとき" do
+      let(:user) { build(:user, cost: 2.0) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:cost]).to include "は整数で入力してください"
+      end
+    end
+  end
 
   subject { user.destroy }
   let(:user) { create(:user) }
