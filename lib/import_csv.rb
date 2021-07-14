e49a5c04450709e8aec5a5eebf424a8b78273d5b
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-07-14 20:59:49 +0900

    CSVインポート機能の実装

---
+++ lib/import_csv.rb
@@ -0,0 +1,22 @@
+require "csv"
+
+class ImportCsv
+  # CSVデータのパスを引数として受け取り、インポート処理を実行
+  def self.import(path)
+    list = []
+
+    CSV.foreach(path, headers: true) do |row|
+      list << row.to_h
+    end
+
+    list
+  end
+
+  def self.information_data
+    list = import('db/csv_data/information.csv')
+
+    puts "インポート処理を開始"
+    Information.create!(list)
+    puts "インポート完了!"
+  end
+end
