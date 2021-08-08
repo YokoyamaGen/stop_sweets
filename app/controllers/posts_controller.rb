a9acb564d192b207eba7c9930035dabe9aeb9fda
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-08-08 11:04:48 +0900

    つぶやき一覧における検索機能の仕様を変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,11 @@ class PostsController < ApplicationController
   def index
     @q = Post.ransack(params[:q])
     @posts = @q.result.includes(:user, :likes).order(created_at: "DESC").page(params[:page]).per(PER_PAGE)
-    flash.now[:alert] = "検索に一致するつぶやきはありませんでした" if @posts.empty? && params[:q].present?
+    # つぶやきが1件も投稿されていなかった場合につぶやき一覧以外のページから遷移された際にメッセージが出力されることを防ぐ
+    if request.referer&.include?("posts")
+      # 検索バーにキーワードが入力されている状態かつ検索結果がなかった場合にメッセージを出力させる
+      flash.now[:alert] = "検索に一致するつぶやきはありませんでした" if @posts.empty? && params[:q][:content_cont].present?
+    end
   end
 
   def new
