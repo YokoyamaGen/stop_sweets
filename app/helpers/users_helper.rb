24781c37f8a35e86378547f6bdd3b176c47d3f9f
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-11-05 22:06:56 +0900

    ビューに記載していたロジックをヘルパーに移動

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,4 +9,11 @@ module UsersHelper
       image_tag '王冠 銅.jpeg'
     end
   end
+
+  def check_user_rank(rank, i, before_user_continue_day, rank_status)
+    if i == 1 || @before_user_continue_day != rank[1]
+      @rank_status = i
+      @before_user_continue_day = rank[1]
+    end
+  end
 end
