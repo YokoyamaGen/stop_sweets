14127af447ca127f0962571f3b4a5e60df33ff1c
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-11-07 15:06:23 +0900

    404、500エラーが発生した際にエラーハンドリングするように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   protected
 
@@ -9,5 +11,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :cost])
     # アカウント編集時のストロングパラメータ
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :cost])
+  end
+
+  private
+
+  def render_500
+    render file: Rails.root.join('public/500.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
   end
 end
